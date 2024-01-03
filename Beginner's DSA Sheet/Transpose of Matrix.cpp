360043cb200ff56d822f3d0a8b99b398bd96a8e0
krishsahu09 <krishsahu0906@gmail.com>
2024-01-03 21:36:12 +0530

    created Transpose Of Matrix.cpp

---
+++ Beginner's DSA Sheet/Transpose of Matrix.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    // Function to find transpose of a matrix.
+    void transpose(vector<vector<int>> &matrix, int n)
+    {
+        int temp;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+};
